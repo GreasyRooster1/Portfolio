32200613f2ea496e0584b381b7bca1b76a2a2a8c
GreasyRooster <greasyRooster1@gmail.com>
2026-07-27 20:17:11 -0700

    simple bento

--- src/FeaturedHero.jsx
+++ src/FeaturedHero.jsx
@@ -2,7 +2,7 @@ import Placeholder from '@components/Placeholder'
 import LinkRow from '@components/LinkRow'
 import styles from './FeaturedHero.module.css'
 import HeroOverlay from "@/HeroOverlay.jsx";
-import {Flex, Grid} from "@radix-ui/themes";
+import {Box, Flex, Grid} from "@radix-ui/themes";
 import React from "react";
 
 const LINKS = [{ label: 'live demo', accent: true }, { label: 'source' }, { label: 'writeup' }]
@@ -10,30 +10,19 @@ const LINKS = [{ label: 'live demo', accent: true }, { label: 'source' }, { labe
 export default function FeaturedHero() {
   return (
     <section className={styles.section}>
-      <Grid style={{height:"50vh"}} columns={{initial:"1",sm:"1",lg:"4"}} gap={{initial:"5",lg:"7"}} >
+      <Grid style={{height:"60vh"}} columns={{initial:"1",sm:"1",lg:"3"}} gap={{initial:"2",lg:"3"}} >
 
-        <Flex className={styles.box} gridRow={"1/4"} gridColumn={"1/4"} justify="end" alignItems="center" direction="column" p={"2"}>
-          <div className={styles.eyebrow}>featured · 2024-2026</div>
-          <div className={styles.title}>Teaching Students</div>
-          <p className={styles.desc}>
-            Currently teaching Javascript, Arduino, Scratch and Python to students ages 6-16
-          </p>
-          <LinkRow links={LINKS} className={styles.links} />
+        <Flex className={styles.box} gridRow={"1/1"} gridColumn={"1/3"}>
+          a
         </Flex>
-        <Flex className={styles.box} gridRow={"1/2"} >
-          d
+        <Flex className={styles.box} gridRow={"1/1"} gridColumn={"3/3"}>
+          a
         </Flex>
-        <Flex className={styles.box} gridRow={"2/5"} gridColumn={"4/5"}>
-          ddd
+        <Flex className={styles.box} gridRow={"2/2"} gridColumn={"2/4"}>
+          a
         </Flex>
-        <Flex className={styles.box} gridRow={"4/6"} gridColumn={"1/3"} >
-          dd
-        </Flex>
-        <Flex className={styles.box} gridRow={"4/6"} gridColumn={"3/4"} >
-          d
-        </Flex>
-        <Flex className={styles.box} gridRow={"5/6"} gridColumn={"4/5"} >
-          d
+        <Flex className={styles.box} gridRow={"2/2"} gridColumn={"1/1"}>
+          a
         </Flex>
 
       </Grid>
