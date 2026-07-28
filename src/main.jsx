b81ef19599971e29b290aebc998f0243a206c283
GreasyRooster <greasyRooster1@gmail.com>
2026-07-27 19:56:57 -0700

    theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import "@radix-ui/themes/styles.css";
 import './index.css'
 import App from './App.jsx'
+import {Theme} from "@radix-ui/themes";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <Theme appearance="dark">
+        <App />
+      </Theme>
   </StrictMode>,
 )
