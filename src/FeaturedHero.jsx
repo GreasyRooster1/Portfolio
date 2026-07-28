fa5100f6b8a3011ef83986620f9e6ced3fc06abb
GreasyRooster <greasyRooster1@gmail.com>
2026-07-27 19:52:30 -0700

    bento layoud

--- src/FeaturedHero.jsx
+++ src/FeaturedHero.jsx
@@ -4,31 +4,30 @@ import styles from './FeaturedHero.module.css'
 import HeroOverlay from "@/HeroOverlay.jsx";
 import {Flex, Grid} from "@radix-ui/themes";
 
-const LINKS = [{ label: 'live demo', accent: true }, { label: 'source' }, { label: 'writeup' }]
-
 export default function FeaturedHero() {
   return (
     <section className={styles.section}>
-      <Grid style={{aspectRatio:"3/2"}} columns={{initial:"1",sm:"1",lg:"3"}} gap={{initial:"5",lg:"7"}} >
-        <Flex className={styles.box}>
-          sadd1
+      <Grid style={{height:"50vh"}} columns={{initial:"1",sm:"1",lg:"4"}} gap={{initial:"5",lg:"7"}} >
+
+        <Flex className={styles.box} gridRow={"1/4"} gridColumn={"1/4"}>
+          d
+        </Flex>
+        <Flex className={styles.box} gridRow={"1/2"} >
+          d
+        </Flex>
+        <Flex className={styles.box} gridRow={"2/5"} gridColumn={"4/5"}>
+          ddd
+        </Flex>
+        <Flex className={styles.box} gridRow={"4/6"} gridColumn={"1/3"} >
+          dd
+        </Flex>
+        <Flex className={styles.box} gridRow={"4/6"} gridColumn={"3/4"} >
+          d
+        </Flex>
+        <Flex className={styles.box} gridRow={"5/6"} gridColumn={"4/5"} >
+          d
         </Flex>
 
-        <Flex className={styles.box}>
-          sadd1
-        </Flex>
-        <Flex className={styles.box}>
-          sadd1
-        </Flex>
-        <Flex className={styles.box}>
-          sadd1
-        </Flex>
-        <Flex className={styles.box}>
-          sadd1
-        </Flex>
-        <Flex className={styles.box}>
-          sadd1
-        </Flex>
       </Grid>
     </section>
   )
--- src/HeroOverlay.jsx
+++ src/HeroOverlay.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Flex, Grid} from "@radix-ui/themes";
 import Placeholder from "@components/Placeholder.jsx";
-import styles from "./heroOverlay.css";
+import styles from "./heroOverlay.module.css";
 import LinkRow from "@components/LinkRow.jsx";
+
+const LINKS = [{ label: 'live demo', accent: true }, { label: 'source' }, { label: 'writeup' }]
 
 function HeroOverlay(props) {
     return (
