cc716705370c539a26368c6604ab445db1acb3cc
GreasyRooster <greasyRooster1@gmail.com>
2026-08-08 15:51:40 -0700

    project index

--- src/ProjectIndex.jsx
+++ src/ProjectIndex.jsx
@@ -1,12 +1,11 @@
 import SectionHead from '@components/SectionHead'
 import IndexRow from '@components/IndexRow'
 import styles from './ProjectIndex.module.css'
-import { INDEX_ROWS } from '@/data'
 
-export default function ProjectIndex() {
+export default function ProjectIndex(props) {
   return (
     <section id="index" className={styles.section}>
-      <SectionHead title="Everything else" meta="8 projects · full index" />
+      <SectionHead title="Everything else" meta={`${props.projects.length} projects`} />
       <div className={styles.header}>
         <div>№</div>
         <div>project</div>
@@ -14,8 +13,8 @@ export default function ProjectIndex() {
         <div>result</div>
         <div className={styles.linksHead}>links</div>
       </div>
-      {INDEX_ROWS.map((r, i) => (
-        <IndexRow key={r.n} {...r} last={i === INDEX_ROWS.length - 1} />
+      {props.projects.map((project, i) => (
+        <IndexRow key={project.n} {...project} last={i === props.projects.length - 1} />
       ))}
     </section>
   )
