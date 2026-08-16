72f83c2affc56750abda52bf7304786b48abfa57
GreasyRooster <greasyRooster1@gmail.com>
2026-08-16 14:17:31 -0700

    array logic

--- src/components/BrickList.jsx
+++ src/components/BrickList.jsx
@@ -1,15 +1,26 @@
 import styles from "./brickList.module.css"
 import React, {Children} from "react";
 
+function fillEmpty(c){
+    return new Array(c).fill(-1)
+}
+
 export function BrickList(props) {
     const childrenArray = Children.toArray(props.children);
-    let rows = [new Array(props.cols).fill(-1),new Array(props.cols).fill(0),new Array(props.cols).fill(-1)];
+    let rows = [fillEmpty(props.cols),fillEmpty(props.cols),fillEmpty(props.cols)];
     if(childrenArray.length <= props.cols-2){
-
+        let diff = (props.cols-childrenArray.length)
+        let offset = Math.floor(diff/2);
+        let row = fillEmpty(offset);
+        row.push(...childrenArray)
+        if(diff%2===1){
+            row.push(...fillEmpty(offset+1))
+        }
+        rows[1] = row;
     }else{
 
     }
-    console.log(childrenArray);
+    console.log(childrenArray,rows);
     return (
         <div className={styles.wall} >
             {rows.map((row, i) => (
