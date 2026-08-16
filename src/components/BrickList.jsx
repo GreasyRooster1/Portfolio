e66c89f0b5681a3d9e88d41641aaebe683343df1
GreasyRooster <greasyRooster1@gmail.com>
2026-08-16 13:50:10 -0700

    item

--- src/components/BrickList.jsx
+++ src/components/BrickList.jsx
@@ -1,9 +1,16 @@
 import styles from "./brickList.module.css"
+import React from "react";
 
 export function BrickList() {
     return (
         <div className={styles.wall}>
-            <div className={styles.row}></div>
+            <div className={styles.row}>
+
+            </div>
         </div>
     )
+}
+
+BrickList.Item = (props) => {
+    return React.cloneElement(props.item, {className:`${props.className},${styles.item}`})
 }
