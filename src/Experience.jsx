dac6239993101757a03c56298382b7aec4611d78
GreasyRooster <greasyRooster1@gmail.com>
2026-08-17 09:48:28 -0700

    two

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,6 +2,7 @@ import styles from "@/ProjectIndex.module.css";
 import SectionHead from "@components/SectionHead.jsx";
 import IndexRow from "@components/IndexRow.jsx";
 import {BrickList, BrickListItem} from "@components/BrickList.jsx";
+import {Flex, Text} from "@radix-ui/themes";
 import rust from "@assets/langs/rustcrab.png"
 import react from "@assets/langs/react.svg"
 import ts from "@assets/langs/ts.jpg"
@@ -19,7 +20,10 @@ import arduino from "@assets/langs/arduino.svg"
 export function Experience(props) {
     return (
         <section className={styles.section} {...props}>
-            <SectionHead title="Experience"/>
+            <SectionHead title="Experience" meta="what i've worked with "/>
+            <Flex width={"100%"} align={"center"} justify={"center"} mb="4" mt="6">
+                <Text width={"100%"} align="center" size="8" weight="bold">Languages</Text>
+            </Flex>
             <BrickList cols={8}>
                 <BrickListItem img={ruby} color={"#d92007"}/>
                 <BrickListItem img={react} color={"#00d8ff"}/>
@@ -35,6 +39,25 @@ export function Experience(props) {
                 <BrickListItem img={css} color={"#1c88c7"}/>
                 <BrickListItem img={swift} color={"#fa5d2d"}/>
             </BrickList>
+            <Flex width={"100%"} align={"center"} justify={"center"} mb="4" mt="6">
+                <Text width={"100%"} align="center" size="8" weight="bold">Technologies</Text>
+            </Flex>
+            <BrickList cols={8}>
+                <BrickListItem img={ruby} color={"#d92007"}/>
+                <BrickListItem img={react} color={"#00d8ff"}/>
+                <BrickListItem img={js} color={"#f0dc4e"}/>
+                <BrickListItem img={ts} color={"#3178c6"}/>
+                <BrickListItem img={html} color={"#e44d26"}/>
+                <BrickListItem img={arduino} color={"#199aa0"}/>
+                <BrickListItem img={c} color={"#00599c"}/>
+                <BrickListItem img={csharp} color={"#953dac"}/>
+                <BrickListItem img={python} color={"#ffcf42"}/>
+                <BrickListItem img={java} color={"#f3931b"}/>
+                <BrickListItem img={rust} color={"#dd3516"}/>
+                <BrickListItem img={css} color={"#1c88c7"}/>
+                <BrickListItem img={swift} color={"#fa5d2d"}/>
+            </BrickList>
+
         </section>
     )
 }
