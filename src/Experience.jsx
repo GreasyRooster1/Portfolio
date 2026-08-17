203e4c167f158f31b36969dfa9c6a6db265858ab
GreasyRooster <greasyRooster1@gmail.com>
2026-08-17 10:13:58 -0700

    tech

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -17,6 +17,19 @@ import ruby from "@assets/langs/ruby.svg"
 import css from "@assets/langs/css.svg"
 import arduino from "@assets/langs/arduino.svg"
 
+import aws from "@assets/langs/aws.png"
+import cloudflare from "@assets/langs/cloudflare.png"
+import docker from "@assets/langs/docker.webp"
+import git from "@assets/langs/git.svg"
+import github from "@assets/langs/github.webp"
+import node from "@assets/langs/node.svg"
+import ollama from "@assets/langs/ollama.png"
+import postgres from "@assets/langs/postgres.svg"
+import proxmox from "@assets/langs/proxmox.png"
+import rpi from "@assets/langs/rpi.svg"
+import firebase from "@assets/langs/firebase.png"
+import vite from "@assets/langs/vite.svg"
+
 export function Experience(props) {
     return (
         <section className={styles.section} {...props}>
@@ -42,20 +55,20 @@ export function Experience(props) {
             <Flex width={"100%"} align={"center"} justify={"center"} mb="4" mt="6">
                 <Text width={"100%"} align="center" size="8" weight="bold">Technologies</Text>
             </Flex>
-            <BrickList cols={8}>
-                <BrickListItem img={ruby} color={"#d92007"}/>
-                <BrickListItem img={react} color={"#00d8ff"}/>
-                <BrickListItem img={js} color={"#f0dc4e"}/>
-                <BrickListItem img={ts} color={"#3178c6"}/>
-                <BrickListItem img={html} color={"#e44d26"}/>
-                <BrickListItem img={arduino} color={"#199aa0"}/>
-                <BrickListItem img={c} color={"#00599c"}/>
-                <BrickListItem img={csharp} color={"#953dac"}/>
-                <BrickListItem img={python} color={"#ffcf42"}/>
-                <BrickListItem img={java} color={"#f3931b"}/>
-                <BrickListItem img={rust} color={"#dd3516"}/>
-                <BrickListItem img={css} color={"#1c88c7"}/>
-                <BrickListItem img={swift} color={"#fa5d2d"}/>
+            <BrickList cols={12}>
+                <BrickListItem img={aws} color={"#d92007"}/>
+                <BrickListItem img={cloudflare} color={"#d92007"}/>
+                <BrickListItem img={docker} color={"#d92007"}/>
+                <BrickListItem img={git} color={"#d92007"}/>
+                <BrickListItem img={github} color={"#d92007"}/>
+                <BrickListItem img={ollama} color={"#d92007"}/>
+                <BrickListItem img={postgres} color={"#d92007"}/>
+                <BrickListItem img={node} color={"#d92007"}/>
+                <BrickListItem img={proxmox} color={"#d92007"}/>
+                <BrickListItem img={rpi} color={"#d92007"}/>
+                <BrickListItem img={firebase} color={"#d92007"}/>
+                <BrickListItem img={vite} color={"#d92007"}/>
+
             </BrickList>
 
         </section>
