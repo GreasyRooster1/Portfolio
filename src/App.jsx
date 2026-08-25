eab6d78cc896fe3dde64a556f4bc2356cff16e39
GreasyRooster <greasyRooster1@gmail.com>
2026-08-24 21:13:02 -0700

    import md

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Index from "@/pages/index/Index.jsx";
 import WriteUp from "@/pages/writeup/WriteUp.jsx";
 
-import qcode_md from "@assets/writeup_qcode.md"
+import qcode_md from "@assets/writeup_qcode.md?raw"
 import qcode_meta from "@assets/writeup_qcode.json"
 
 export default function App() {
--- src/pages/writeup/WriteUp.jsx
+++ src/pages/writeup/WriteUp.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Markdown from "react-markdown";
 
 function WriteUp(props) {
     return (
         <div className="page">
-
+            <Markdown>
+                {props.md}
+            </Markdown>
         </div>
     );
 }
