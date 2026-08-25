5e0ade8844f4114063e16767d8713c290b6f7770
GreasyRooster <greasyRooster1@gmail.com>
2026-08-24 21:28:59 -0700

    style

--- src/pages/writeup/WriteUp.jsx
+++ src/pages/writeup/WriteUp.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Markdown from "react-markdown";
+import styles from "./writeUp.module.css";
+import {DesktopView} from "@components/View.jsx";
 
 function WriteUp(props) {
     return (
-        <div className="page">
+        <div className={styles.page}>
+            <DesktopView>
+                <div className={styles.menuWrapper}>
+                    <div className={styles.menu}>
+                        asd
+                    </div>
+                </div>
+            </DesktopView>
             <Markdown>
                 {props.md}
             </Markdown>
