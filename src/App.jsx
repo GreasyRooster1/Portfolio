0aca43d72897bf552808f2cf702d7556d4410dc5
GreasyRooster <greasyRooster1@gmail.com>
2026-08-24 21:03:35 -0700

    meta

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import Index from "@/pages/index/Index.jsx";
 import WriteUp from "@/pages/writeup/WriteUp.jsx";
 
+import qcode_md from "@assets/writeup_qcode.md"
+import qcode_meta from "@assets/writeup_qcode.json"
+
 export default function App() {
     return (
         <Routes>
             <Route exact path="/" element={<Index/>} />
-            <Route exact path="/writeup" element={<WriteUp content={}/>} />
+            <Route exact path="/writeup" element={<WriteUp md={qcode_md} meta={qcode_meta}/>} />
         </Routes>
     )
 }
