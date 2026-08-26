59fb3695923a9feb4bf773beb1a91856f5364a0e
GreasyRooster <greasyRooster1@gmail.com>
2026-08-25 17:37:01 -0700

    use meta title

--- src/pages/writeup/WriteUp.jsx
+++ src/pages/writeup/WriteUp.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Markdown from "react-markdown";
 import styles from "./writeUp.module.css";
 import {DesktopView} from "@components/View.jsx";
+import HtmlTitle from "@components/HtmlTitle.jsx";
 
 function WriteUp(props) {
     return (
         <div className={styles.page}>
+            <HtmlTitle title={props.meta.title}/>
             <DesktopView>
                 <div className={styles.menuWrapper}>
                     <div className={styles.menu}>
