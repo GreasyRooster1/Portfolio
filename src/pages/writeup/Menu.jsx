d3e14f074598a563eb3a697eef21c46adc58b8da
GreasyRooster <greasyRooster1@gmail.com>
2026-08-25 17:54:08 -0700

    asidfh

--- src/pages/writeup/Menu.jsx
+++ src/pages/writeup/Menu.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {DesktopView} from "@components/View.jsx";
 import styles from "./writeUp.module.css";
+import {Flex} from "@radix-ui/themes";
 
 function Menu(props) {
     return (
         <DesktopView>
             <div className={styles.menuWrapper}>
-                <div className={styles.menu}>
-                    asd
-                </div>
+                <Flex direction="column" gapY="2" className={styles.menu}>
+                    <span className={styles.menuItem}>Item</span>
+                    <span className={styles.menuItem}>Item</span>
+                    <span className={styles.menuItem}>Item</span>
+                    <span className={styles.menuItem}>Item</span>
+                </Flex>
             </div>
         </DesktopView>
     );
