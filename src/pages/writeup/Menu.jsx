e4550829293129dfacd50bb5d5102d0e8f2a4b25
GreasyRooster <greasyRooster1@gmail.com>
2026-08-25 17:38:51 -0700

    yank menu

---
+++ src/pages/writeup/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {DesktopView} from "@components/View.jsx";
+import styles from "./writeUp.module.css";
+
+function Menu(props) {
+    return (
+        <DesktopView>
+            <div className={styles.menuWrapper}>
+                <div className={styles.menu}>
+                    asd
+                </div>
+            </div>
+        </DesktopView>
+    );
+}
+
+export default Menu;
--- src/pages/writeup/WriteUp.jsx
+++ src/pages/writeup/WriteUp.jsx
@@ -3,18 +3,13 @@ import Markdown from "react-markdown";
 import styles from "./writeUp.module.css";
 import {DesktopView} from "@components/View.jsx";
 import HtmlTitle from "@components/HtmlTitle.jsx";
+import Menu from "@/pages/writeup/Menu.jsx";
 
 function WriteUp(props) {
     return (
         <div className={styles.page}>
             <HtmlTitle title={props.meta.title}/>
-            <DesktopView>
-                <div className={styles.menuWrapper}>
-                    <div className={styles.menu}>
-                        asd
-                    </div>
-                </div>
-            </DesktopView>
+            <Menu {...props}/>
             <Markdown>
                 {props.md}
             </Markdown>
