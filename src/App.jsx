62eb5950b5cf3ad0e346724e9c1ae023b37d66b9
GreasyRooster <greasyRooster1@gmail.com>
2026-08-26 20:25:15 -0700

    qc projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 import Index from "@/pages/index/Index.jsx";
+import QCodeProjects from "@/pages/qcodeProjects/QCodeProjects.jsx";
 
 export default function App() {
     return (
         <Routes>
             <Route exact path="/" element={<Index/>} />
+            <Route exact path="/qcode_projects" element={<QCodeProjects/>} />
         </Routes>
     )
 }
---
+++ src/pages/qcodeProjects/QCodeProjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QCodeProjects(props) {
+    return (
+        <div className="page">
+
+        </div>
+    );
+}
+
+export default QCodeProjects;
