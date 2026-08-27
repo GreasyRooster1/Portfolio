35fda25d3dcceb61a809f61ec6c2cd3cb57777b5
GreasyRooster <greasyRooster1@gmail.com>
2026-08-26 21:38:52 -0700

    test

--- src/pages/qcodeProjects/QCodeProjects.jsx
+++ src/pages/qcodeProjects/QCodeProjects.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import QCProject from "@/pages/qcodeProjects/QCProject.jsx";
 
 function QCodeProjects(props) {
     return (
         <div className="page">
-
+            <QCProject id={422378628263191}/>
         </div>
     );
 }
