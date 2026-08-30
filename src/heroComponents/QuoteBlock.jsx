a96bedc308f1d6757f18d2f5ee0de502c776362f
GreasyRooster <greasyRooster1@gmail.com>
2026-08-30 11:56:51 -0700

    button

--- src/heroComponents/QuoteBlock.jsx
+++ src/heroComponents/QuoteBlock.jsx
@@ -8,17 +8,17 @@ import esporterzImg from '@assets/esporterz3.png'
 export function QuoteBlock(props) {
     return (
         <Flex className={styles.box} justify={"center"} align={"center"} direction={"column"} style={{fontSize:"14pt",overflow:"clip",position:"relative",zIndex:"1"}} {...props}>
-            <CarouselPrimitive autoTime={5000_000000}>
+            <CarouselPrimitive autoTime={30_000} noArrows>
                 <HeroQuote author={(<>
                     Sally Newson, Founder, <a href={"https://karma-club.org/"}>Karma Club & The Kove</a>
                 </>)} src={karmaImg}>
                     Dillon was one of the most impressive student instructors I've ever seen. His ability to explain complex coding concepts with patience, clarity, and enthusiasm kept every student fully engaged from start to finish. Quite honestly, he taught with more skill and confidence than many professional educators. He has a remarkable gift for teaching and inspiring young people, and we were incredibly fortunate to have him lead our Coding Camp.
                 </HeroQuote>
-                <HeroQuote author={(<>
-                    Nadia Berkowitz, Founder & Owner, <a href={"https://esporterz.com"}>Esporterz</a>
-                </>)} src={esporterzImg} imgClass={styles.esporterz}>
-                    Dillon was one of the most impressive student instructors I've ever seen. His ability to explain complex coding concepts with patience, clarity, and enthusiasm kept every student fully engaged from start to finish. Quite honestly, he taught with more skill and confidence than many professional educators. He has a remarkable gift for teaching and inspiring young people, and we were incredibly fortunate to have him lead our Coding Camp.
-                </HeroQuote>
+                {/*<HeroQuote author={(<>*/}
+                {/*    Nadia Berkowitz, Founder & Owner, <a href={"https://esporterz.com"}>Esporterz</a>*/}
+                {/*</>)} src={esporterzImg} imgClass={styles.esporterz}>*/}
+                {/*    Dillon was one of the most impressive student instructors I've ever seen. His ability to explain complex coding concepts with patience, clarity, and enthusiasm kept every student fully engaged from start to finish. Quite honestly, he taught with more skill and confidence than many professional educators. He has a remarkable gift for teaching and inspiring young people, and we were incredibly fortunate to have him lead our Coding Camp.*/}
+                {/*</HeroQuote>*/}
             </CarouselPrimitive>
         </Flex>
     )
