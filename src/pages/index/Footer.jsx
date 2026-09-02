8542ff69b6c0001e8eb030552253c5318d9cd7d8
GreasyRooster <greasyRooster1@gmail.com>
2026-09-01 17:38:41 -0700

    make email more readable

--- src/pages/index/Footer.jsx
+++ src/pages/index/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <Flex px={{initial:"8px",sm:"28px"}} py={{initial:"18px",sm:"64px"}} className={styles.footer}>
       <div className={styles.email}>
-        dillonjwilson@gmail.com
+        DillonJWilson1@gmail.com
         <Cursor />
       </div>
         <div><a href={"https://github.com/GreasyRooster1"}>github</a> / <a href={"mailto:dillonjwilson1@gmail.com"}>email</a></div>
