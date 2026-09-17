33cc74ae7e4571a4fef7c204d6db54a5ff7bf5a2
GreasyRooster <greasyRooster1@gmail.com>
2026-09-17 16:46:32 -0700

    add titles dynamically

--- src/pages/writeup/WriteUp.jsx
+++ src/pages/writeup/WriteUp.jsx
@@ -4,13 +4,28 @@ import styles from "./writeUp.module.css";
 import {DesktopView} from "@components/View.jsx";
 import HtmlTitle from "@components/HtmlTitle.jsx";
 import Menu from "@/pages/writeup/Menu.jsx";
+import remarkGfm from 'remark-gfm'
 
 function WriteUp(props) {
+    const [titles, setTitles] = React.useState([]);
+
+    const addTitle = (title) => {
+        setTitles((prevTitles) => [...prevTitles, title]);
+    }
+
     return (
         <div className={styles.page}>
             <HtmlTitle title={props.meta.title}/>
-            <Menu {...props}/>
-            <Markdown>
+            <Menu titles={titles} {...props}/>
+            <Markdown components={{
+                h1(props) {
+                    const {node, ...rest} = props
+                    let title = node.innerHTML
+                    let id = `${title.toLowerCase()}${titles.length}`;
+                    addTitle({name:title,id:id})
+                    return <h1 style={{color: 'red'}} {...rest} />
+                }
+            }}>
                 {props.md}
             </Markdown>
         </div>
